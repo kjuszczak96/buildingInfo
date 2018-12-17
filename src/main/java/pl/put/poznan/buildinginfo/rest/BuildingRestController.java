c21a0252ea4e1e5d51466e68bce43a452e1381ca
Kamil Juszczak <kjuszczak96@gmail.com>
2018-12-17 20:40:25 +0100

    Add documentation and setup gui connection

--- src/main/java/pl/put/poznan/buildinginfo/rest/BuildingRestController.java
+++ src/main/java/pl/put/poznan/buildinginfo/rest/BuildingRestController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.put.poznan.buildinginfo.models.Building;
 
+/**
+ * Representation of a spring REST controller for Buildings.
+ * 
+ * @since 0.2
+ * @see BuildingInfoController
+ * @see Building
+ */
 @RestController
 @RequestMapping("/building")
 public class BuildingRestController extends BuildingInfoController<Building> {}
--- src/main/java/pl/put/poznan/buildinginfo/rest/LevelRestController.java
+++ src/main/java/pl/put/poznan/buildinginfo/rest/LevelRestController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.put.poznan.buildinginfo.models.Level;
 
+/**
+ * Representation of a spring REST controller for Levels.
+ * 
+ * @since 0.2
+ * @see BuildingInfoController
+ * @see Level
+ */
 @RestController
 @RequestMapping("/level")
 public class LevelRestController extends BuildingInfoController<Level> {}
